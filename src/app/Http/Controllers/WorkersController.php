903571a83419c971a2840fc9634e2888b8f1859f
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-19 17:20:26 -0300

    feat: added some new methods to Workers

--- src/app/Http/Controllers/WorkersController.php
+++ src/app/Http/Controllers/WorkersController.php
@@ -3,17 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 use App\Models\Workers;
 
 class WorkersController extends Controller
 {
-    public function __construct()
-    {
-
-    }
+    public function __construct() {}
 
     public function getAllWorkers()
     {
         return Workers::all();
     }
+
+	public function getWorker(int $id)
+	{
+		$worker = null;
+
+		try
+		{
+			$worker = Workers::findOrFail($id);
+		} catch(ModelNotFoundException $e)
+		{
+			return response()->json(
+				[
+					"operation_status" => "500",
+					"errorType" => $e->getMessage(),
+				]
+			);
+		}
+
+		return response()->json(
+			[
+				"operation_status" => "200",
+				"savedData" => $worker
+			]
+		);
+	}
+
+	public function postWorker(Request $req)
+	{
+		// TODO: add error handling
+		$worker = new Workers();
+
+		$worker->name = $req->input("workerName", null);
+		$worker->salary = $req->input("workerSalary", 0.00);
+		$worker->contractStart = $req->input("workerStart", null);
+		$worker->contractEnd = $req->input("workerEnd", null);
+		$worker->save();
+
+		return response()->json(
+			[
+				"operation_status" => "200",
+				"savedData" => $worker
+			]
+		);
+	}
 }
