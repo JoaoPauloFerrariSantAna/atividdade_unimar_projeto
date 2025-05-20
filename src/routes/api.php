913debff4a8bfe557371a77bc9ccbde2d3841d33
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-20 16:23:48 -0300

    feat: added new routes for departament and workers

--- src/routes/api.php
+++ src/routes/api.php
@@ -11,7 +11,9 @@ use App\Http\Controllers\WorkersController;
 
 Route::get("/departament", array(DepartamentController::class, "getAllDepartaments"));
 Route::get("/departament/{id}", array(DepartamentController::class, "getDepartament"));
+# Route::get("/departament/worker/{workerId}", array(DepartamentController::class, "getWorkerDepartament"));
 Route::post("/departament", array(DepartamentController::class, "postDepartament"));
+Route::patch("/departament/{id}", array(DepartamentController::class, "patchDepartament"));
 
 # endregion Departaments
 
