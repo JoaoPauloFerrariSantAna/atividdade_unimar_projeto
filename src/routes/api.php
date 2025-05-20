a42881943c1383db6c8ed2b38abec013d035ee12
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-20 20:09:30 -0300

    feat: added new route

--- src/routes/api.php
+++ src/routes/api.php
@@ -9,19 +9,19 @@ use App\Http\Controllers\WorkersController;
 
 # region Departaments
 
-Route::get("/departament", array(DepartamentController::class, "getAllDepartaments"));
-Route::get("/departament/{id}", array(DepartamentController::class, "getDepartament"));
-# Route::get("/departament/worker/{workerId}", array(DepartamentController::class, "getWorkerDepartament"));
-Route::post("/departament", array(DepartamentController::class, "postDepartament"));
+Route::get("/departament/worker/{workerId}", array(DepartamentController::class, "getWorkerDepartament"));
 Route::patch("/departament/{id}", array(DepartamentController::class, "patchDepartament"));
+Route::get("/departament/{id}", array(DepartamentController::class, "getDepartament"));
+Route::get("/departament", array(DepartamentController::class, "getAllDepartaments"));
+Route::post("/departament", array(DepartamentController::class, "postDepartament"));
 
 # endregion Departaments
 
 # region Workers
 
-Route::get("/worker", array(WorkersController::class, "getAllWorkers"));
 Route::get("/worker/{id}", array(WorkersController::class, "getWorker"));
-Route::post("/worker", array(WorkersController::class, "postWorker"));
 Route::patch("/worker/{id}", array(WorkersController::class, "patchWorker"));
+Route::get("/worker", array(WorkersController::class, "getAllWorkers"));
+Route::post("/worker", array(WorkersController::class, "postWorker"));
 
 # endregion Workers
