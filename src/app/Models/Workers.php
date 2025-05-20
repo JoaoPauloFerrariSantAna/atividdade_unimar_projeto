736e2895c06aca309a249a8064a3611e99315d7a
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-20 08:40:51 -0300

    fix: updated case
    (this problem is quite common with me, i prefer snake_case ob SQL and camelCase on OOP code)

--- src/app/Models/Workers.php
+++ src/app/Models/Workers.php
@@ -8,10 +8,10 @@ use App\Models\Departament;
 class Workers extends Model
 {
     protected $table = "worker_tbl";
-    protected $fillable = array("name", "salary", "departament_id", "contract_start", "contract_end");
+    protected $fillable = array("name", "salary", "contractStart", "contractEnd");
 
     public function departament()
     {
-        return $this->hasOne("departament_id", Departament::class);
+        return $this->hasOne("departamentId", Departament::class);
     }
 }
