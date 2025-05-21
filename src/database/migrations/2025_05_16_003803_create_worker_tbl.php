bc3328adcffcbf6345ba03372f8330afe8e87fec
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-21 15:32:09 -0300

    feat: modified migration to delete dept data

--- src/database/migrations/2025_05_16_003803_create_worker_tbl.php
+++ src/database/migrations/2025_05_16_003803_create_worker_tbl.php
@@ -15,7 +15,19 @@ return new class extends Migration
             $table->id();
             $table->string("name", DEFAULT_NAME_SIZE)->nullable(false);
             $table->double("salary")->nullable(false)->default(DEFAULT_SALARY);
-            $table->foreignId("departamentId")->constrained()->references("id")->on("departament_tbl");
+
+			# NOTE: from what i could think of:
+			# it would be better to put workerId (this table's id) with departamentId
+			# on another table in this case, it would simplify all of the logic a little
+			# but i don't have enough time to be doin' that
+			# i've wasted enough in ASM and some shortcomings in this code are because of that.
+
+			# https://laravel.com/docs/7.x/migrations#foreign-key-constraints
+
+			# TODO: the thing is: if we delete with cascade, we'll lose every worker
+			# how to fix that...?
+            $table->foreignId("departamentId")->constrained()
+				->references("id")->on("departament_tbl")->onDelete("cascade");
             $table->dateTime("contractStart")->nullable(false);
             $table->dateTime("contractEnd")->nullable(true);
             $table->timestamps();
