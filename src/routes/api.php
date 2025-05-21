dd437ac53623d8f087395393ec5052434e9dfb4c
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-21 15:41:01 -0300

    ref: grouped routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,14 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DepartamentController;
 use App\Http\Controllers\WorkersController;
 
-# TODO: group it
+Route::controller(DepartamentController::class)->group(function() {
+	Route::get("/departament/worker/{workerId}", "getWorkerDepartament");
+	Route::patch("/departament/{id}", "patchDepartament");
+	Route::delete("/departament/{id}", "deleteDepartament");
+	Route::get("/departament/{id}", "getDepartament");
+	Route::get("/departament", "getAllDepartaments");
+	Route::post("/departament", "postDepartament");
+});
 
-Route::get("/departament/worker/{workerId}", array(DepartamentController::class, "getWorkerDepartament"));
-Route::patch("/departament/{id}", array(DepartamentController::class, "patchDepartament"));
-Route::delete("/departament/{id}", array(DepartamentController::class, "deleteDepartament"));
-Route::get("/departament/{id}", array(DepartamentController::class, "getDepartament"));
-Route::get("/departament", array(DepartamentController::class, "getAllDepartaments"));
-Route::post("/departament", array(DepartamentController::class, "postDepartament"));
+# TODO: group it
 
 Route::get("/worker/{id}", array(WorkersController::class, "getWorker"));
 Route::patch("/worker/{id}", array(WorkersController::class, "patchWorker"));
