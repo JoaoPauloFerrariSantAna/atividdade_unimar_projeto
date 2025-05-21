01b16eb04d624ad98bf145dc910cdc20162666f0
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-21 15:31:17 -0300

    feat: added delete route

--- src/routes/api.php
+++ src/routes/api.php
@@ -7,21 +7,14 @@ use App\Http\Controllers\WorkersController;
 
 # TODO: group it
 
-# region Departaments
-
 Route::get("/departament/worker/{workerId}", array(DepartamentController::class, "getWorkerDepartament"));
 Route::patch("/departament/{id}", array(DepartamentController::class, "patchDepartament"));
+Route::delete("/departament/{id}", array(DepartamentController::class, "deleteDepartament"));
 Route::get("/departament/{id}", array(DepartamentController::class, "getDepartament"));
 Route::get("/departament", array(DepartamentController::class, "getAllDepartaments"));
 Route::post("/departament", array(DepartamentController::class, "postDepartament"));
-
-# endregion Departaments
-
-# region Workers
 
 Route::get("/worker/{id}", array(WorkersController::class, "getWorker"));
 Route::patch("/worker/{id}", array(WorkersController::class, "patchWorker"));
 Route::get("/worker", array(WorkersController::class, "getAllWorkers"));
 Route::post("/worker", array(WorkersController::class, "postWorker"));
-
-# endregion Workers
